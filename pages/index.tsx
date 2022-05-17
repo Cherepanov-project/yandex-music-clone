494a8aa6e2548dcabaf6e8ba592f10036fb52f77
Sergey Rogozhin <sergeyrogojin@gmail.com>
2022-05-17 13:44:17 +0700

    add slider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 
+import {Slider} from "./slider/slider";
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <h1>Yandex-Music Clone</h1>
+      <Slider />
     </div>
   );
 };
