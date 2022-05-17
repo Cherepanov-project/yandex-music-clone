27cbf5d5d0f6e749818918c5c1b313dbb9118b0c
Sergey Rogozhin <sergeyrogojin@gmail.com>
2022-05-17 15:54:31 +0700

    add slider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 
-import {Slider} from "./slider/slider";
+import {Slider} from "./slider";
 
 const Home: NextPage = () => {
   return (
