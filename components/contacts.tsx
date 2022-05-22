be1721a8ae732a596ccc191cd3111d76e3609b42
Alena <babankova.alena1991@gmail.com>
2022-05-22 05:32:17 +0300

    fix babel conflict

--- components/contacts.tsx
+++ components/contacts.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 
   const contacts = [
-    {id: 1, src: "/location.png", alt: "location", title: "Our Location", descr: "The Queen's Walk, Bishop's,", descr2:"London SE1 7PB, UnitedKingdom",href:"/"},
+    {id: 1, src: "/location.png", alt: "location", title: "Our Location", descr: "The Queen's Walk, Bishop's,", descr2:"London SE1 7PB, UnitedKingdom",href:""},
     {id: 2, src: "/phone.png", alt: "phone", title: "Call Us At", descr: "+ (567) 1234-567-8900",descr2: "+ (567) 1234-567-8901", href:"tel:+ (567) 1234-567-8900"},
     {id: 3, src: "/mail.png", alt: "email", title: "Mail Us", descr: "info@gentechtree.com", descr2: "info2@gentechtree.com", href:"mailto:infogentechtree2@gmail.com"},
  ];
