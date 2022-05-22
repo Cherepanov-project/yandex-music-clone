193e451ef7eb1d971dced90413815af1db74e34b
Alena <babankova.alena1991@gmail.com>
2022-05-23 01:11:41 +0300

    got rid duplication in the table

--- pages/pricingTable.tsx
+++ pages/pricingTable.tsx
@@ -1,108 +1,57 @@
 import Image from "next/image";
-import check from "../img/check.png";
 
 import { TbContainer, Table, BtnTable } from "../styles/pricingTableStyle";
+const trText = [
+  { id: 1, title: "", td1: "BASIC", td2: "STANDART", td3: "PREMIUM" },
+  { id: 2, title: "", td1: "0$", td2: "0$", td3: "0$" },
+  { id: 3, title: "Number Of Screen", td1: "1", td2: "2", td3: "4" },
+  {
+    id: 4,
+    title: "On how many device you can Download",
+    td1: "1",
+    td2: "2",
+    td3: "4",
+  },
+];
+const trCheck = [
+  { id: 5, title: "Unlimited TV shows and movies", src: "/check.png" },
+  { id: 6, title: "watch on mobile and tablet", src: "/check.png" },
+  { id: 7, title: "watch on laptop and tv", src: "/check.png" },
+  { id: 8, title: "HD available", src: "/check.png" },
+  { id: 9, title: "ultra HD available", src: "/check.png" },
+];
+
 const PricingTable = () => {
   return (
     <>
       <TbContainer>
         <Table>
           <tbody>
+            {trText.map(({ id, title, td1, td2, td3 }) => (
+              <tr key={id}>
+                <td>{title}</td>
+                <td>{td1}</td>
+                <td>{td2}</td>
+                <td>{td3}</td>
+              </tr>
+            ))}
+            {trCheck.map(({ id, title, src }) => (
+              <tr key={id}>
+                <td>{title}</td>
+                {[...Array(3)].map((_, i) => (
+                  <td key={i}>
+                    <Image src={src} alt="check" width={20} height={20} />
+                  </td>
+                ))}
+              </tr>
+            ))}
             <tr>
               <td></td>
-              <td>BASIC</td>
-              <td>STANDART</td>
-              <td>PREMIUM</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td>0$</td>
-              <td>0$</td>
-              <td>0$</td>
-            </tr>
-            <tr>
-              <td>Number Of Screen</td>
-              <td>1</td>
-              <td>2</td>
-              <td>4</td>
-            </tr>
-            <tr>
-              <td>On how many device you can Download</td>
-              <td>1</td>
-              <td>2</td>
-              <td>4</td>
-            </tr>
-            <tr>
-              <td>Unlimited TV shows and movies</td>
-              <td>
-                <Image src={check} alt="check" width={20} height={20} />
-              </td>
-              <td>
-                <Image src={check} alt="check" width={20} height={20} />
-              </td>
-              <td>
-                <Image src={check} alt="check" width={20} height={20} />
-              </td>
-            </tr>
-            <tr>
-              <td>watch on mobile and tablet</td>
-              <td>
-                <Image src={check} alt="check" width={20} height={20} />
-              </td>
-              <td>
-                <Image src={check} alt="check" width={20} height={20} />
-              </td>
-              <td>
-                <Image src={check} alt="check" width={20} height={20} />
-              </td>
-            </tr>
-            <tr>
-              <td>watch on laptop and tv</td>
-              <td>
-                <Image src={check} alt="check" width={20} height={20} />
-              </td>
-              <td>
-                <Image src={check} alt="check" width={20} height={20} />
-              </td>
-              <td>
-                <Image src={check} alt="check" width={20} height={20} />
-              </td>
-            </tr>
-            <tr>
-              <td>HD available</td>
-              <td>
-                <Image src={check} alt="check" width={20} height={20} />
-              </td>
-              <td>
-                <Image src={check} alt="check" width={20} height={20} />
-              </td>
-              <td>
-                <Image src={check} alt="check" width={20} height={20} />
-              </td>
-            </tr>
-            <tr>
-              <td>ultra HD available</td>
-              <td>
-                <Image src={check} alt="check" width={20} height={20} />
-              </td>
-              <td>
-                <Image src={check} alt="check" width={20} height={20} />
-              </td>
-              <td>
-                <Image src={check} alt="check" width={20} height={20} />
-              </td>
-            </tr>
-            <tr>
-              <td></td>
-              <td>
-                <BtnTable type="button">SUBSCRIBE</BtnTable>
-              </td>
-              <td>
-                <BtnTable type="button">SUBSCRIBE</BtnTable>
-              </td>
-              <td>
-                <BtnTable type="button">SUBSCRIBE</BtnTable>
-              </td>
+              {[...Array(3)].map((_, i) => (
+                <td key={i}>
+                  <BtnTable type="button">SUBSCRIBE</BtnTable>
+                </td>
+              ))}
             </tr>
           </tbody>
         </Table>
